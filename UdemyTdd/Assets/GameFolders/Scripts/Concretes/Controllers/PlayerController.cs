74b74378cbf06b718f09b55b713d0fdd0641de1a
berkterek <42406263+berkterek@users.noreply.github.com>
2021-09-04 22:58:52 +0300

    Feat:Core Files and assembly definition controllers and movements

---
+++ UdemyTdd/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyTdd.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }    
+}
+
---
+++ UdemyTdd/Assets/GameFolders/Scripts/Concretes/Movements/MoveWithTranslate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyTdd.Movements
+{
+    public class MoveWithTranslate : MonoBehaviour
+    {
+        
+    }    
+}
+
